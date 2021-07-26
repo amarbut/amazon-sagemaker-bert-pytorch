599ab43fa923f99444e666920501db782f281170
Anna Marbut <anna.marbut@submittable.com>
2021-07-26 16:44:29 -0600

    Debugging indexing error

--- code/train_deploy.py
+++ code/train_deploy.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
-MAX_LEN = 1986  # this is the max length of the sentence
+MAX_LEN = 351  # this is the max length of the sentence
 
 print("Loading BERT tokenizer...")
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", do_lower_case=True)
